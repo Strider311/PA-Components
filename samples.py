1a97c58a41b883045971e775dafae90d0dd0d3fc
Saif <saif_kh31@outlook.com>
2023-12-22 09:56:31 +0400

    Update

--- samples.py
+++ samples.py
@@ -1,3 +1,4 @@
+import os
 from MultispectralProcessing.MultispectralProcessing import Multispectral
 
 """This section is sample code for multispectral processing
@@ -8,7 +9,11 @@ multispectralProcessor = Multispectral("C:\\Users\\saif_\\output")
 nir_img_path = "C:\\Users\\saif_\\Main\\Source\\PrecisionAgriculture\\Data\\Input\\Near_Infrared_Channel\\Train_Images\\Image_001.jpg"
 red_img_path = "C:\\Users\\saif_\\Main\\Source\\PrecisionAgriculture\\Data\\Input\\Red_Channel\\Train_Images\\Image_001.jpg"
 
-ndvi = multispectralProcessor.ndvi(nir_img=nir_img_path, red_img=red_img_path)
+image_list = os.listdir("path")
+
+for image in image_list:
+    ndvi = multispectralProcessor.ndvi(nir_img=nir_img_path, red_img=red_img_path)
+    
 
 """ Image Processing using OpenCv
 """
@@ -17,7 +22,7 @@ from ImageProcessingApproach.OpenCvMultispectralClassifier import OpenCvClassifi
 output_dir = "C:\\Users\\saif_\\output"
 
 # soil_max=0.1, unhealthy_max=0.45 are the values used in the report
-multispectralOpenCvClassifier = OpenCvClassifier(0.1, 0.45, "ndvi", output_dir)
+multispectralOpenCvClassifier = OpenCvClassifier(0.1, 0.6, "ndvi", output_dir)
 
 ndvi_file_path = output_dir + "Image_001.txt"
 
@@ -25,3 +30,26 @@ result = multispectralOpenCvClassifier.apply_filter(ndvi_file_path)
 percent_healthy = result.healthy_percent
 unhealthy_percent = result.unhealthy_percent
 soil_percent = result.soil_percent
+
+""" Yolov8 training
+"""
+
+from ultralytics import YOLO
+ 
+# Load the model.
+model = YOLO('yolov8n.pt') # -> models can be changed based on hardware, ex: yolov8s.pt, yolov8m.pt
+ 
+# to use cuda
+# model.to('cuda')
+
+# Training.
+results = model.train(
+   data='pothole_v8.yaml',
+   imgsz=1280,
+   epochs=50,
+   batch=8,
+   name='yolov8n_v8_50e'
+)
+
+# https://docs.ultralytics.com/modes/predict/#arguments
+# model.predict(source, save=True, imgsz=320, conf=0.5,device='xyz')
